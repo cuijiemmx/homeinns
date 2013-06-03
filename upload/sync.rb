c502185f334cd3974650395d4755a2e280c03538
casa <cuijiemmx@163.com>
2013-06-03 16:29:13 +0800

    switch back to sifang.com

--- upload/sync.rb
+++ upload/sync.rb
@@ -1,10 +1,8 @@
 #! /usr/bin/env ruby
 
-address = '192.168.1.99'
-#account = 'www-data'
-account = 'eng'
-#path = '/var/www/html'
-path = '/home/eng'
+address = 'eng.corp.sifang.com'
+account = 'www-data'
+path = '/var/www/html'
 src = 'homeinns'
 command = "rsync #{ARGV} -k -rvz --delete --delete-excluded --exclude '.*' #{src} #{account}@#{address}:#{path}"
 puts `#{command}`
