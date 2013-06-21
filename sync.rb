b4e1822ce28b9a6f8632c0e8dbe0c04737ffff69
casa <cuijiemmx@163.com>
2013-06-21 15:50:33 +0800

    update sync.rb

--- sync.rb
+++ sync.rb
@@ -4,4 +4,4 @@ address = 'eng.corp.sifang.com'
 account = 'www-data'
 dest = '/var/www/html/homeinns'
 command = "rsync #{ARGV} -k -rvz --delete --delete-excluded --exclude '.*' upload/ #{account}@#{address}:#{dest}"
-puts `#{command}`
+system(command)
